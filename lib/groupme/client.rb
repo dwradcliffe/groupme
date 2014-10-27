410739c121de48126fa1e405dafe614763bb3dd5
David Radcliffe <radcliffe.david@gmail.com>
2014-10-27 09:20:17 -0400

    RuboCop: Style/IndentationConsistency

--- lib/groupme/client.rb
+++ lib/groupme/client.rb
@@ -22,38 +22,38 @@ module GroupMe
 
     private
 
-      def get(path)
-        request(:get, path)
+    def get(path)
+      request(:get, path)
+    end
+
+    def post(path, data={})
+      request(:post, path, data)
+    end
+
+    def request(method, path, data={})
+      res = connection.send(method, "v3/#{path}", data)
+      if res.success? and !res.body.empty?
+        return res.body.response
+      else
+        return res
       end
+    end
 
-      def post(path, data={})
-        request(:post, path, data)
+    # Returns a Faraday::Connection object
+    #
+    # @return [Faraday::Connection]
+    def connection
+      @connection ||= Faraday.new 'https://api.groupme.com/' do |f|
+        f.request :json
+        f.headers[:user_agent] = GroupMe::USER_AGENT
+        f.headers["X-Access-Token"] = @token
+
+        # f.response :logger
+        f.response :mashify
+        f.response :json, :content_type => /\bjson$/
+
+        f.adapter Faraday.default_adapter
       end
-
-      def request(method, path, data={})
-        res = connection.send(method, "v3/#{path}", data)
-        if res.success? and !res.body.empty?
-          return res.body.response
-        else
-          return res
-        end
-      end
-
-      # Returns a Faraday::Connection object
-      #
-      # @return [Faraday::Connection]
-      def connection
-        @connection ||= Faraday.new 'https://api.groupme.com/' do |f|
-          f.request :json
-          f.headers[:user_agent] = GroupMe::USER_AGENT
-          f.headers["X-Access-Token"] = @token
-
-          # f.response :logger
-          f.response :mashify
-          f.response :json, :content_type => /\bjson$/
-
-          f.adapter Faraday.default_adapter
-        end
-      end
+    end
   end
 end
