1845adac1667592a71012ad3368fc1d8c48f2c49
Brandon Hilkert <brandonhilkert@gmail.com>
2016-03-15 14:00:52 -0400

    Dig in to direct messages on response

--- spec/groupme/direct_messages_spec.rb
+++ spec/groupme/direct_messages_spec.rb
@@ -11,7 +11,7 @@ describe GroupMe::DirectMessages do
     it "returns direct messages between two users" do
       stub_get('/direct_messages?other_user_id=1234567890').to_return(json_response('direct_messages.json'))
       direct_messages = @client.direct_messages(1234567890)
-      expect(direct_messages.direct_messages.first.name).to eq('John')
+      expect(direct_messages.first.name).to eq('John')
     end
 
   end
